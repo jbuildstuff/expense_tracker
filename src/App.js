11bb5075600f73b42737dd25907f0be0306fc9dd
jbuildstuff <jlee651@illinois.edu>
2023-02-24 15:06:25 -0600

    re organizing folders compomenets

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
-import Card from "./components/Card";
-import Expenses from "./components/Expenses";
+import Card from "./components/UI/Card";
+import Expenses from "./components/Expenses/Expenses";
 
 function App() {
   const expenses = [
