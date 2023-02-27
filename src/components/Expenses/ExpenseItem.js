cb35730ee2b2be93ab19159eba6918dd390cbd7f
jbuildstuff <jlee651@illinois.edu>
2023-02-27 12:34:31 -0600

    adding states

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,26 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import React, { useState } from "react";
 import Card from "../UI/Card";
+import { click } from "@testing-library/user-event/dist/click";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+  console.log("ExpenseItem evaluated by React");
+
+  const clickHanlder = () => {
+    setTitle("Updated");
+    console.log(title);
+  };
+
   return (
-    <Card className="expense-item">
+    <Card className="expensew-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
+      <button onClick={clickHanlder}>Change Title</button>
     </Card>
   );
 };
