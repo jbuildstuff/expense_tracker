8ff7425ff99d6b4fd3fe49efbff936146f33ae27
jbuildstuff <jlee651@illinois.edu>
2023-03-01 17:02:56 -0600

    add dynamically adding list

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import "./App.css";
 import Card from "./components/UI/Card";
-
+import React, { useState } from "react";
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
-
+const INITIAL_EXPENSES = [
+  { title: "Car Insurance", amount: 29257.11, date: new Date(2021, 2, 28) },
+  { title: "Karate Lesson", amount: 5297.11, date: new Date(2020, 3, 25) },
+  { title: "Massage Chair", amount: 1297.11, date: new Date(2011, 6, 11) },
+  { title: "Beauty Products", amount: 2397.11, date: new Date(2021, 5, 1) },
+];
 const App = () => {
-  const expenses = [
-    { title: "Car Insurance", amount: 29257.11, date: new Date(2021, 2, 28) },
-    { title: "Karate Lesson", amount: 5297.11, date: new Date(2020, 3, 25) },
-    { title: "Massage Chair", amount: 1297.11, date: new Date(2011, 6, 11) },
-    { title: "Beauty Products", amount: 2397.11, date: new Date(2021, 5, 1) },
-  ];
+  const [expenses, setExpenses] = useState(INITIAL_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log("In App Js");
-    console.log(expenses);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
